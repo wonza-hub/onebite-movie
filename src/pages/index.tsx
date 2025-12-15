df532a993673c5bfc90820dc8fd1be7d23c14196
wonza-hub <wonza8585@gmail.com>
2025-12-15 14:43:05 +0900

    feat: 메인페이지 추가

---
+++ src/pages/index.tsx
@@ -0,0 +1,57 @@
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode, useEffect } from "react";
+import MovieItem from "@/components/movie-item";
+import style from "./index.module.css";
+import { InferGetServerSidePropsType } from "next";
+import fetchMovies from "@/lib/fetch-movies";
+import fetchRandomMovies from "@/lib/fetch-random-movies";
+
+export const getServerSideProps = async () => {
+  const [allMovies, recommendedMovies] = await Promise.all([
+    fetchMovies(),
+    fetchRandomMovies(),
+  ]);
+
+  return {
+    props: {
+      allMovies,
+      recommendedMovies,
+    },
+  };
+};
+
+export default function Home({
+  allMovies,
+  recommendedMovies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(allMovies);
+
+  useEffect(() => {
+    console.log(window);
+  }, []);
+
+  return (
+    <div>
+      <section>
+        <h3>지금 추천하는 영화</h3>
+        <div className={style.recommend_movie_list}>
+          {recommendedMovies.slice(0, 3).map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+      <section>
+        <h3>등록된 모든 영화</h3>
+        <div className={style.all_movie_list}>
+          {allMovies.map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
