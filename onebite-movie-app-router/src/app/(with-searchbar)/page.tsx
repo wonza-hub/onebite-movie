d06a40241f8c47a81075ae8de4e8f0f09e697380
wonza-hub <wonza8585@gmail.com>
2025-12-29 14:11:59 +0900

    feat: 영화 목록/검색 페이지에 스트리밍 적용

--- onebite-movie-app-router/src/app/(with-searchbar)/page.tsx
+++ onebite-movie-app-router/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,14 @@
 import MovieItem from "@/components/movie-item";
 import style from "./page.module.css";
 import { MovieData } from "@/types";
+import { delay } from "@/utils/delay";
+import { Suspense } from "react";
+import MovieListSkeleton from "@/components/skeleton/movie-list-skeleton";
+
+export const dynamic = "force-dynamic";
 
 async function AllMovies() {
+  await delay(2000);
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/movie`, {
     cache: "force-cache",
   });
@@ -22,6 +28,7 @@ async function AllMovies() {
 }
 
 async function RecommendMovies() {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/movie/random`,
     { next: { revalidate: 3 } }
@@ -47,13 +54,17 @@ export default function Page() {
       <section>
         <h3>지금 추천하는 영화</h3>
         <div className={style.recommend_movie_list}>
-          <RecommendMovies />
+          <Suspense fallback={<MovieListSkeleton count={3} />}>
+            <RecommendMovies />
+          </Suspense>
         </div>
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
         <div className={style.all_movie_list}>
-          <AllMovies />
+          <Suspense fallback={<MovieListSkeleton count={5} />}>
+            <AllMovies />
+          </Suspense>
         </div>
       </section>
     </div>
--- onebite-movie-app-router/src/app/(with-searchbar)/search/page.tsx
+++ onebite-movie-app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,13 @@
 import MovieItem from "@/components/movie-item";
 import style from "./page.module.css";
 import { MovieData } from "@/types";
+import { delay } from "@/utils/delay";
+import { Suspense } from "react";
+import MovieListSkeleton from "@/components/skeleton/movie-list-skeleton";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ q: string }>;
-}) {
-  const { q } = await searchParams;
+async function SearchResult({ q }: { q: string }) {
+  await delay(1500);
+  console.log("delay");
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/movie/search?q=${q}`,
     { cache: "force-cache" }
@@ -16,15 +16,25 @@ export default async function Page({
     return <div>Failed to fetch movies</div>;
   }
   const movies: MovieData[] = await response.json();
+
   if (movies.length === 0) {
     return <div>No movies found</div>;
   }
 
+  return movies.map((movie) => <MovieItem key={movie.id} {...movie} />);
+}
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+
   return (
     <div className={style.container}>
-      {movies.map((movie) => (
-        <MovieItem key={movie.id} {...movie} />
-      ))}
+      <Suspense key={q || ""} fallback={<MovieListSkeleton count={10} />}>
+        <SearchResult q={q || ""} />
+      </Suspense>
     </div>
   );
 }
