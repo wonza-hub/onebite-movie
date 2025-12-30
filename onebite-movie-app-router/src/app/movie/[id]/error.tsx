af83530821bc87538eb4141369a5f96fe25aaade
wonza-hub <wonza8585@gmail.com>
2025-12-30 12:31:22 +0900

    feat: 에러 페이지 추가

---
+++ onebite-movie-app-router/src/app/movie/[id]/error.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>Error: {error.message}</p>
+      <button
+        onClick={() => {
+          startTransition(() => {
+            router.refresh();
+            reset();
+          });
+        }}
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
