971df2cbe8704408f52d8f995f1cfef91fc9b519
wonza-hub <wonza8585@gmail.com>
2025-12-31 21:53:59 +0900

    feat: 리뷰 작성 결과 useActionState로 관리하는 기능 추가

--- onebite-movie-app-router/src/actions/create-review.action.ts
+++ onebite-movie-app-router/src/actions/create-review.action.ts
@@ -2,24 +2,40 @@
 
 import { revalidateTag } from "next/cache";
 
-export default async function createReviewAction(formData: FormData) {
+export default async function createReviewAction(_: any, formData: FormData) {
   const movieId = formData.get("movieId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!content || !author || !movieId) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
   }
 
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/review`, {
-      method: "POST",
-      body: JSON.stringify({ movieId, content, author }),
-    });
-
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/review/1`,
+      {
+        method: "POST",
+        body: JSON.stringify({ movieId, content, author }),
+      }
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     revalidateTag(`review-${movieId}`);
+
+    return {
+      status: true,
+      error: "",
+    };
   } catch (error) {
     console.error(error);
-    return;
+    return {
+      status: false,
+      error: `리뷰 작성에 실패했습니다. ${error}`,
+    };
   }
 }
--- onebite-movie-app-router/src/components/review-editor.tsx
+++ onebite-movie-app-router/src/components/review-editor.tsx
@@ -1,15 +1,41 @@
+"use client";
+
+import { useActionState, useEffect } from "react";
 import style from "./review-editor.module.css";
 import createReviewAction from "@/actions/create-review.action";
 
 export default function ReviewEditor({ movieId }: { movieId: string }) {
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null
+  );
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
   return (
     <section>
-      <form className={style.form_container} action={createReviewAction}>
+      <form className={style.form_container} action={formAction}>
         <input name="movieId" value={movieId} hidden readOnly />
-        <textarea required name="content" placeholder="리뷰 내용" />
+        <textarea
+          disabled={isPending}
+          required
+          name="content"
+          placeholder="리뷰 내용"
+        />
         <div className={style.submit_container}>
-          <input required name="author" placeholder="작성자" />
-          <button type="submit">작성하기</button>
+          <input
+            disabled={isPending}
+            required
+            name="author"
+            placeholder="작성자"
+          />
+          <button type="submit" disabled={isPending}>
+            {isPending ? "..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
